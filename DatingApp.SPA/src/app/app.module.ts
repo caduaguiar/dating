8df31aa8be3e3f1646a2adb94b8f4186323669d1
Kadu - home <kaduaguiar.dev@gmail.com>
2018-01-06 17:25:43 -0300

    Add router

--- DatingApp.SPA/src/app/app.module.ts
+++ DatingApp.SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 // Componets
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -15,16 +16,26 @@ import { AlertifyService } from './_services/alertify.service';
 
 // Thred parties
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
+// Router
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
 ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpModule,
     FormsModule,
